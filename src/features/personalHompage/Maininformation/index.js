3d9ab3b68d81a636a5a85053e07a5d286c330ec2
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-04-14 20:52:29 +0200

    Maininformation added

---
+++ src/features/personalHompage/Maininformation/index.js
@@ -0,0 +1,29 @@
+import { email } from "../email";
+import MateuszPiaszczykProfile from "./profile.webp";
+import {
+  Avatar,
+  ButtonIcon,
+  FullName,
+  StyledButtonLink,
+  Summary,
+  ThisIs,
+  Wrapper,
+} from "./styled";
+
+export const MainInformation = () => {
+  <Wrapper>
+    <Avatar src={MateuszPiaszczykProfile} alt="Mateusz Piaszczyk" />
+    <div>
+      <ThisIs>This is</ThisIs>
+      <FullName>Mateusz Piaszczyk</FullName>
+      <Summary>
+        👨🏻‍💻 I’m a passionate Frontend Developer in love with React,currently
+        looking for new job opportunities.
+      </Summary>
+      <StyledButtonLink href={`mailto:${email}`} title={email}>
+        <ButtonIcon />
+        Hire Me
+      </StyledButtonLink>
+    </div>
+  </Wrapper>;
+};
