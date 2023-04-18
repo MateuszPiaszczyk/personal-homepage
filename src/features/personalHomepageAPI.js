81f366b7cbb02b998e9fd9376ffd00f936fbd2d7
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-04-18 23:02:03 +0200

    axios get only gh-pages repositories

--- src/features/personalHomepageAPI.js
+++ src/features/personalHomepageAPI.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const githubAPI_URL = "https://api.github.com";
 
-export const getRepositories = username =>
- axios.get(`${githubAPI_URL}/users/${username}/repos`)
-    .then(response => response.data);
+export const getRepositories = (username) =>
+  axios.get(`${githubAPI_URL}/users/${username}/repos`).then((response) => {
+    const repositories = response.data;
+    return repositories.filter((repo) => repo.has_pages);
+  });
